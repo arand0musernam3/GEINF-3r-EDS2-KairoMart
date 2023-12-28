203095d4269f5af0deb972706dcaa19ca9c14282
UdG <udg@debian.debian>
2023-12-28 20:06:46 +0100

    adaptability

--- Kairomart/src/AdaptabilityDecorator.java
+++ Kairomart/src/AdaptabilityDecorator.java
@@ -1,2 +1,39 @@
-public class AdaptabilityDecorator {
+
+public class AdaptabilityDecorator extends MotorDecorator {
+
+	private float _adaptability;
+
+	public AdaptabilityDecorator(IMotor motor, float adaptability) {
+		super(motor);
+
+		_adaptability = adaptability;
+
+	}
+
+	@Override
+	public float accelerate(float speed, float by) {
+
+		float inc = super.accelerate(speed, by);
+
+		return inc * _adaptability;
+	}
+
+	@Override
+	public float turn(float angle, float by) {
+
+		float inc = super.turn(angle, by);
+
+		return inc * _adaptability;
+	}
+
+	@Override
+	public Vec2f move(float speed, float angle) {
+
+		Vec2f inc = super.move(speed, angle);
+		inc.x *= _adaptability;
+		inc.y *= _adaptability;
+
+		return inc;
+	}
+
 }
