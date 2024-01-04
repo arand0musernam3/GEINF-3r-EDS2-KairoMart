3d07259e4fc474124b91b300fef879f0b407fa0f
Jordi Badia <jordibadiauladell@gmail.com>
2024-01-04 20:44:36 +0100

    Atre mes

--- Kairomart/src/Race.java
+++ Kairomart/src/Race.java
@@ -3,11 +3,11 @@ import java.util.*;
 public class Race {
 
     Race() {
-        _terrains = new ArrayList<>();
-        _terrains.add(Terrain.ROAD, new Terrain("Road", "Recently asphalted road."));
-        _terrains.add(Terrain.MUD, new Terrain("Mud", "Slippery and gets your car dirty."));
+        ArrayList<Terrain> terrains = new ArrayList<>();
+        terrains.add(Terrain.ROAD, new Terrain("Road", "Recently asphalted road."));
+        terrains.add(Terrain.MUD, new Terrain("Mud", "Slippery and gets your car dirty."));
 
-        _track = new Track("Computer Science Plains", 5, _terrains);
+        _track = new Track("Computer Science Plains", 5, terrains);
 
         _characters = new ArrayList<>();
         _players = new ArrayList<>();
@@ -20,7 +20,7 @@ public class Race {
         // Terrain adaptabilities
         for (int i = 0; i < 2; i++)
             for (int j = 1; j <= 3; j++)
-                TerrainAdaptability.addAdaptability(_terrains.get(i), motorSwitch(j), ((float)j*(i+1))/6); //Per dir algo, fantasia
+                TerrainAdaptability.addAdaptability(terrains.get(i), motorSwitch(j), ((float)j*(i+1))/6); //Per dir algo, fantasia
     }
 
     public void addCharacter(String name, String desc, float q_adapt, float b_adapt, float h_adapt) {
@@ -34,25 +34,22 @@ public class Race {
     public void addPlayer(String name, int i_character, int i_vehicle) {
 
         Character c = _characters.get(i_character-1);
-        Motor motor = motorSwitch(i_vehicle);
+        IMotor motor = motorSwitch(i_vehicle);
+
+        motor = new AdaptabilityDecorator(motor,CharacterAdaptability.getAdaptability(c, motor));
+        AdaptabilityDecorator terrain_adaptability = new AdaptabilityDecorator(motor,1); // Snup doggggg
+
         Vehicle v = new Vehicle(motor);
 
         _players.add(new Player(name, c, v));
 
-        IMotor m = v.getMotor();
-        v.setWrappee(new AdaptabilityDecorator(m, CharacterAdaptability.getAdaptability(c, motor)));
-
-        m = v.getMotor();
-        AdaptabilityDecorator terrain_adaptability = new AdaptabilityDecorator(m, TerrainAdaptability.getAdaptability(_track.getTerrainByPosition(v.position()),motor));
-
         _terrain_control.put(v,terrain_adaptability);
-        v.setWrappee(terrain_adaptability);
     }
 
     public void updateVehicle(int i_player, int factor, int move_type) {
         if (move_type == 1)
             _players.get(i_player-1).vehicle().accelerate(factor);
-        else
+        else if (move_type == 2)
             _players.get(i_player-1).vehicle().turn(factor);
     }
 
@@ -95,12 +92,11 @@ public class Race {
         }
     }
 
-    private ArrayList<Terrain> _terrains;
-    private Track _track;
-    private ArrayList<Character> _characters;
-    private ArrayList<Player> _players;
+    private final Track _track;
+    private final ArrayList<Character> _characters;
+    private final ArrayList<Player> _players;
 
-    private HashMap<Vehicle, AdaptabilityDecorator> _terrain_control;
+    private final HashMap<Vehicle, AdaptabilityDecorator> _terrain_control;
 
     private final Quadriga _quadriga;
     private final Biga _biga;
--- Kairomart/src/TerrainAdaptability.java
+++ Kairomart/src/TerrainAdaptability.java
@@ -3,12 +3,12 @@ import java.util.HashMap;
 
 public final class TerrainAdaptability {
 
-    private static HashMap<Terrain, HashMap<Motor, Float>> _adaptabilities = new HashMap<>();
+    private static HashMap<Terrain, HashMap<IMotor, Float>> _adaptabilities = new HashMap<>();
 
     /**
      * Adds or overwrites the current Adaptability of Terrain t and Motor m with the parameter value.
      */
-    public static void addAdaptability(Terrain t, Motor m, float value) {
+    public static void addAdaptability(Terrain t, IMotor m, float value) {
         if (!_adaptabilities.containsKey(t)) {
             _adaptabilities.put(t, new HashMap<>());
         }
@@ -18,7 +18,7 @@ public final class TerrainAdaptability {
     /**
      * Returns the adaptability between Terrain t and Motor m.
      */
-    public static float getAdaptability(Terrain t, Motor m) {
+    public static float getAdaptability(Terrain t, IMotor m) {
         return _adaptabilities.get(t).get(m);
     }
 
